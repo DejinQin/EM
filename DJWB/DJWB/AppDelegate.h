96cc7ff2d7f0a1a928911aa7e19d6e1aaa4aa44c
qindejin <qindejin@qindejin.com>
2018-04-19 13:40:40 +0800

    添加文件

---
+++ DJWB/DJWB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJWB
+//
+//  Created by EM-iMac on 2018/4/17.
+//  Copyright © 2018年 EM-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
